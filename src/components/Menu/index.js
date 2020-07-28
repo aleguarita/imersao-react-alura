de045ab3849990b051601e332df6d4b7db4d62ea
Alessandro Guarita <alessandro.guarita@gmail.com>
2020-07-28 19:24:32 -0300

    Aula 02

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './menu.css'
 import Logo from '../../assets/img/logo.png'
 // import ButtonLink from './components/ButtonLink'
@@ -7,11 +8,11 @@ import Button from '../Button'
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img src={Logo} className="Logo" alt="Aluraflix" title="Logo da Aluraflix" />
-            </a>
+            </Link>
 
-            <Button href="/" className="ButtonLink">
+            <Button as={Link} to="/cadastro/video" className="ButtonLink">
                 Novo Vídeo
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import  { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
+ 
+// views
+import Home from './pages/home';
+import CadastroVideo from './pages/cadastro/video';
+import CadastroCategoria from './pages/cadastro/categoria';
+
+const Pagina404 = () => (<div>Erro 404. Desafio: criar uma página melhor</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+        <Route path="/cadastro/video" component={CadastroVideo} exact />
+        <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
+        <Route path="/" component={Home} exact />
+        <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
